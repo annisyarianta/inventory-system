e3c7f3d7cdc41af29df86bd7ed3d931946184a83
Annisya Rianta <annisyarianta@gmail.com>
2024-08-03 14:26:23 +0700

    edit view update masukga

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -1,95 +1,95 @@
 @extends('layouts.master')
 @section('content')
-@if (session('sukses'))
-<div class="alert alert-success alert-dismissible" role="alert">
-    {{session('sukses')}} <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-            aria-hidden="true">×</span></button>
-</div>
-@endif
-@if (session('gagal'))
-<div class="alert alert-danger alert-dismissible" role="alert">
-    {{session('gagal')}} <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-            aria-hidden="true">×</span></button>
-</div>
-@endif
-<!-- Begin Page Content -->
-<div class="container-fluid">
-    <!-- Page Heading -->
-    <h1 class="h2 mb-2 text-black-800" style="font-weight: 600">Cetak Laporan Transaksi</h1>
-    <!-- Basic Card Example -->
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Form Cetak Laporan</h6>
+    @if (session('sukses'))
+        <div class="alert alert-success alert-dismissible" role="alert">
+            {{ session('sukses') }} <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                    aria-hidden="true">×</span></button>
         </div>
-        <div class="card-body">
-            <!-- Tambahkan id ke form untuk mempermudah manipulasi JavaScript -->
-            <form id="formCetakLaporan" action="" method="POST" enctype="multipart/form-data">
-                {{csrf_field()}}
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="jenislaporan">Jenis Laporan</label>
-                        <select name="jenislaporan" class="form-control" id="jenislaporan">
-                            <option value="">-- Pilih Jenis Laporan --</option>
-                            <option value="barangmasuk">Barang Masuk</option>
-                            <option value="barangkeluar">Barang Keluar</option>
-                        </select>
-                    </div>
+    @endif
+    @if (session('gagal'))
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            {{ session('gagal') }} <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                    aria-hidden="true">×</span></button>
+        </div>
+    @endif
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <h1 class="h2 mb-2 text-black-800" style="font-weight: 600">Cetak Laporan Transaksi</h1>
+        <!-- Basic Card Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Form Cetak Laporan</h6>
+            </div>
+            <div class="card-body">
+                <!-- Tambahkan id ke form untuk mempermudah manipulasi JavaScript -->
+                <form id="formCetakLaporan" action="" method="POST" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="jenislaporan">Jenis Laporan</label>
+                            <select name="jenislaporan" class="form-control" id="jenislaporan">
+                                <option value="">-- Pilih Jenis Laporan --</option>
+                                <option value="barangmasuk">Barang Masuk</option>
+                                <option value="barangkeluar">Barang Keluar</option>
+                            </select>
+                        </div>
 
-                    <div class="form-group{{$errors->has('tanggalawal') ? ' has-error ' : ''}}">
-                        <label for="tanggalawal">Tanggal Awal</label>
+                        <div class="form-group{{ $errors->has('tanggalawal') ? ' has-error ' : '' }}">
+                            <label for="tanggalawal">Tanggal Awal</label>
                             <input name="tanggalawal" type="date" class="form-control" id="tanggalawal"
-                                placeholder="Tanggal Awal" value="{{old('tanggalawal')}}">
-                        @if ($errors->has('tanggalawal'))
-                        <span class="help-block">*Kolom ini harus diisi</span>
-                        @endif
+                                placeholder="Tanggal Awal" value="{{ old('tanggalawal') }}">
+                            @if ($errors->has('tanggalawal'))
+                                <span class="help-block">*Kolom ini harus diisi</span>
+                            @endif
+                        </div>
+
+                        <div class="form-group{{ $errors->has('tanggalakhir') ? ' has-error ' : '' }}">
+                            <label for="tanggalakhir">Tanggal Akhir</label>
+                            <input name="tanggalakhir" type="date" class="form-control" id="tanggalakhir"
+                                placeholder="Tanggal Akhir" value="{{ old('tanggalakhir') }}">
+                            @if ($errors->has('tanggalakhir'))
+                                <span class="help-block">*Kolom ini harus diisi</span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formatdokumen">Format Dokumen</label>
+                            <select name="formatdokumen" class="form-control" id="formatdokumen"
+                                onchange="updateFormAction()">
+                                <option value="">-- Pilih Format Dokumen --</option>
+                                <option value="excel">Excel</option>
+                                <option value="pdf">PDF</option>
+                            </select>
+                        </div>
                     </div>
 
-                    <div class="form-group{{$errors->has('tanggalakhir') ? ' has-error ' : ''}}">
-                        <label for="tanggalakhir">Tanggal Akhir</label>
-                        <input name="tanggalakhir" type="date" class="form-control" id="tanggalakhir" placeholder="Tanggal Akhir"
-                            value="{{old('tanggalakhir')}}">
-                        @if ($errors->has('tanggalakhir'))
-                        <span class="help-block">*Kolom ini harus diisi</span>
-                        @endif
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary btn-icon-split btn-sm">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-print"></i>
+                            </span>
+                            <span class="text">Cetak Laporan</span>
+                        </button>
                     </div>
 
-                    <div class="form-group">
-                        <label for="formatdokumen">Format Dokumen</label>
-                        <select name="formatdokumen" class="form-control" id="formatdokumen" onchange="updateFormAction()">
-                            <option value="">-- Pilih Format Dokumen --</option>
-                            <option value="excel">Excel</option>
-                            <option value="pdf">PDF</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary btn-icon-split btn-sm">
-                        <span class="icon text-white-50">
-                            <i class="fas fa-print"></i>
-                        </span>
-                        <span class="text">Cetak Laporan</span>
-                    </button>
-                </div>
-                
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
-<script>
-function updateFormAction() {
-    var format = document.getElementById('formatdokumen').value;
-    var form = document.getElementById('formCetakLaporan');
+    <script>
+        function updateFormAction() {
+            var format = document.getElementById('formatdokumen').value;
+            var form = document.getElementById('formCetakLaporan');
 
-    if (format === 'excel') {
-        form.action = '/laporan/exportexcellaporan';
-    } else if (format === 'pdf') {
-        form.action = '/laporan/exportpdflaporan';
-    } else {
-        form.action = ''; // Set default atau kosong jika tidak ada format yang dipilih
-    }
-}
-</script>
-
+            if (format === 'excel') {
+                form.action = '/laporan/exportexcellaporan';
+            } else if (format === 'pdf') {
+                form.action = '/laporan/exportpdflaporan';
+            } else {
+                form.action = ''; // Set default atau kosong jika tidak ada format yang dipilih
+            }
+        }
+    </script>
 @endsection
--- resources/views/masukga/edit.blade.php
+++ resources/views/masukga/edit.blade.php
@@ -1,61 +1,63 @@
 @extends('layouts.master')
 @section('content')
-
-<!-- MAIN -->
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel">
-            <div class="panel-heading">
-                <h2>Edit Barang Masuk</h2>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <h1 class="h2 mb-2 text-black-800" style="font-weight: 600">Edit Barang Masuk</h1>
+        <!-- Basic Card Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Form Barang Masuk</h6>
             </div>
-            <div class="panel-body">
-                <form id="aksi" action="/masukga/{{$barangmasuk->id}}/update" method="POST" enctype="multipart/form-data">
-                    {{csrf_field()}}
-                    <div class="form-group">
-                        <label for="barangga_id">Nama Barang</label>
-                        <select name="barangga_id" class="form-control" id="barangga_id">
-                            @foreach ($barangga as $brg)
-                            <option value="{{$brg->id}}" @if ($barangmasuk->barangga_id == $brg->id)
-                                selected
-                                @endif>
-                                {{$brg->namabarang}}
-                            </option>
-                            @endforeach
-                        </select>
-                    </div>
-    
-                    <div class="form-group{{$errors->has('tanggalmasuk') ? ' has-error ' : ''}}">
-                        <label for="tanggalmasuk">Tanggal Barang Masuk</label>
-                            <input name="tanggalmasuk" type="date" class="form-control" id="tanggalmasuk" value="{{$barangmasuk->tanggalmasuk}}">
-                        @if ($errors->has('tanggalmasuk'))
-                        <span class="help-block">{{$errors->first('tanggalmasuk')}}</span>
-                        @endif
-                    </div>
-    
-                    <div class="form-group{{$errors->has('jumlahmasuk') ? ' has-error ' : ''}}">
-                        <label for="jumlahmasuk">Jumlah Barang Masuk</label>
-                        <input name="jumlahmasuk" type="number" class="form-control" id="jumlahmasuk" value="{{$barangmasuk->jumlahmasuk}}">
-                        @if ($errors->has('jumlahmasuk'))
-                        <span class="help-block">{{$errors->first('jumlahmasuk')}}</span>
-                        @endif
+            <div class="card-body">
+                <form id="aksi" action="/masukga/{{ $barangmasuk->id }}/update" method="POST"
+                    enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="barangga_id">Nama Barang</label>
+                            <select name="barangga_id" class="form-control" id="barangga_id">
+                                @foreach ($barangga as $brg)
+                                    <option value="{{ $brg->id }}" @if ($barangmasuk->barangga_id == $brg->id) selected @endif>
+                                        {{ $brg->namabarang }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('tanggalmasuk') ? ' has-error ' : '' }}">
+                            <label for="tanggalmasuk">Tanggal Barang Masuk</label>
+                            <input name="tanggalmasuk" type="date" class="form-control" id="tanggalmasuk"
+                                value="{{ $barangmasuk->tanggalmasuk }}">
+                            @if ($errors->has('tanggalmasuk'))
+                                <span class="help-block">{{ $errors->first('tanggalmasuk') }}</span>
+                            @endif
+                        </div>
+
+                        <div class="form-group{{ $errors->has('jumlahmasuk') ? ' has-error ' : '' }}">
+                            <label for="jumlahmasuk">Jumlah Barang Masuk</label>
+                            <input name="jumlahmasuk" type="number" class="form-control" id="jumlahmasuk"
+                                value="{{ $barangmasuk->jumlahmasuk }}">
+                            @if ($errors->has('jumlahmasuk'))
+                                <span class="help-block">{{ $errors->first('jumlahmasuk') }}</span>
+                            @endif
+                        </div>
+
+                        <div class="form-group{{ $errors->has('satuan') ? ' has-error ' : '' }}">
+                            <label for="satuan">Satuan</label>
+                            <input name="satuan" type="text" class="form-control" id="satuan"
+                                value="{{ $barangmasuk->satuan }}">
+                            @if ($errors->has('satuan'))
+                                <span class="help-block">{{ $errors->first('satuan') }}</span>
+                            @endif
+                        </div>
                     </div>
 
-                    <div class="form-group{{$errors->has('satuan') ? ' has-error ' : ''}}">
-                        <label for="satuan">Satuan</label>
-                        <input name="satuan" type="text" class="form-control" id="satuan" value="{{$barangmasuk->satuan}}">
-                        @if ($errors->has('satuan'))
-                        <span class="help-block">{{$errors->first('satuan')}}</span>
-                        @endif
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-warning">Update</button>
                     </div>
-    
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-warning">Update</button>
                 </form>
             </div>
         </div>
     </div>
-</div>
-
-<!-- END MAIN -->
-
 @endsection
